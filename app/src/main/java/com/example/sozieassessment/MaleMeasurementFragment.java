cf54b92e4936afe470e8518780fad1a874919507
Farhan Ahmed <hfarhan1991@gmail.com>
2020-06-15 16:36:20 +0500

    UI complete

--- app/src/main/java/com/example/sozieassessment/MaleMeasurementFragment.java
+++ app/src/main/java/com/example/sozieassessment/MaleMeasurementFragment.java
@@ -11,26 +11,27 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class MaleMeasurementFragment extends Fragment {
+
 
     RadioGroup measurementGroup;
     RadioButton inchRB,cmRB;
-    EditText heightFeet, heightIn, waistET, hipsET, braBandET, braCupET, dressSizeAndET, topSizeAndET, bottomSizeAndET;
-    TextView unitLabelFT, unitLabelIn, waistMeasureLabelIn, hipsMeasureLabelIn;
+    EditText heightFeet, heightIn, waistET, hipsET, chest;
+    TextView unitLabelFT, unitLabelIn, waistMeasureLabelIn, hipsMeasureLabelIn, chestLabelIn;
     View heightDividerView;
 
     String selectedMeasurement = "inch";
 
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_male_measurement, container, false);
         measurementGroup = view.findViewById(R.id.scaleRG);
         inchRB = view.findViewById(R.id.inchRB);
         cmRB = view.findViewById(R.id.centiRB);
@@ -42,12 +43,14 @@ public class FirstFragment extends Fragment {
         unitLabelIn = view.findViewById(R.id.unitLabelIn);
         waistMeasureLabelIn = view.findViewById(R.id.waistMeasureLabelIn);
         hipsMeasureLabelIn = view.findViewById(R.id.hipsMeasureLabelIn);
+        chestLabelIn = view.findViewById(R.id.chestIn);
 
         //Edittext fields init
         heightFeet = view.findViewById(R.id.heightFeet);
         heightIn = view.findViewById(R.id.heightInch);
         waistET = view.findViewById(R.id.waist);
         hipsET = view.findViewById(R.id.hips);
+        chest = view.findViewById(R.id.chest);
 
         measurementGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -63,6 +66,7 @@ public class FirstFragment extends Fragment {
                     unitLabelIn.setText("in");
                     waistMeasureLabelIn.setText("in");
                     hipsMeasureLabelIn.setText("in");
+                    chestLabelIn.setText("in");
                 }else{
                     measurementGroup.setBackgroundResource(R.drawable.centi_tab);
                     selectedMeasurement = "centi";
@@ -74,6 +78,7 @@ public class FirstFragment extends Fragment {
                     unitLabelIn.setText("cm");
                     waistMeasureLabelIn.setText("cm");
                     hipsMeasureLabelIn.setText("cm");
+                    chestLabelIn.setText("cm");
                 }
             }
         });
@@ -81,22 +86,23 @@ public class FirstFragment extends Fragment {
         return view;
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_MaleMeasurementFragment);
+                NavHostFragment.findNavController(MaleMeasurementFragment.this)
+                        .navigate(R.id.action_MaleFragment_to_FirstFragment);
             }
         });
 
         view.findViewById(R.id.measurementGuideText).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                NavHostFragment.findNavController(MaleMeasurementFragment.this)
+                        .navigate(R.id.action_MaleFragment_to_MaleTipsFragment);
             }
         });
     }
